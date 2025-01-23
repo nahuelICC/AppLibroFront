6bb9935f9a77ee470ab01990bf1545b5f2abe9e7
rafdiecas <rdiezcastro@safareyes.es>
2025-01-23 16:29:37 +0100

    boton to guapo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BotonComponent} from './shared/components/boton/boton.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BotonComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
