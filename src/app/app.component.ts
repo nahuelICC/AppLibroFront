4cad21099e5f37a800d3ed85f83be50fe794760c
Luis Alberto <lmarincarmona@safareyes.es>
2025-01-23 17:10:34 +0100

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {BotonComponent} from './shared/components/boton/boton.component';
+import {FooterComponent} from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BotonComponent],
+  imports: [RouterOutlet, FooterComponent, BotonComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
