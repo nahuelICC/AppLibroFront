d2006920e05d71e69445e52fca405ea2603dc233
Lucia <lmerinodelgado@safareyes.es>
2025-01-27 20:56:36 +0100

    Importaciones para la conexion de frontend con backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {BotonComponent} from './shared/components/boton/boton.component';
-import {FooterComponent} from './shared/components/footer/footer.component';
-import {CuadroProductoComponent} from './features/tienda/components/cuadro-producto/cuadro-producto.component';
+import { BotonComponent } from './shared/components/boton/boton.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CuadroProductoComponent } from './features/tienda/components/cuadro-producto/cuadro-producto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, BotonComponent, CuadroProductoComponent],
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+    BotonComponent,
+    CuadroProductoComponent
+  ],
   templateUrl: './app.component.html',
   standalone: true,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AppLibro';
