58c5fcfd07142cc7121989c91140d2c95fad7849
rafdiecas <rdiezcastro@safareyes.es>
2025-01-29 18:35:51 +0100

    comienzo pagina usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TiendaComponent } from './features/tienda/pages/tienda/tienda.component';
 import { LoginComponent } from './features/login/components/login/login.component';
+import {PaginaUsuarioComponent} from './features/usuario/components/pagina-usuario/pagina-usuario.component';
 
 export const routes: Routes = [
   { path: '', component: TiendaComponent },
   { path: 'login', component: LoginComponent },
   { path: 'main', component: TiendaComponent },
+  { path: 'usuario', component: PaginaUsuarioComponent },
 ];
