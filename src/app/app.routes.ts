015589aaf2eb600d1afe12aa94af4767fccc460a
Lucia <lmerinodelgado@safareyes.es>
2025-01-29 12:36:50 +0100

    ruta de la pagina detalle del libro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {TiendaComponent} from './features/tienda/pages/tienda/tienda.component';
+import {DetallesComponent} from './features/detalles_libro/pages/detalles/detalles.component';
 
 export const routes: Routes = [
-  { path: '', component: TiendaComponent}
+  { path: '', component: TiendaComponent},
+  { path: 'detallelibro/:id', component: DetallesComponent}
 ];
