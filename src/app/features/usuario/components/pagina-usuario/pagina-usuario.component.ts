bfa86a58f2dbd2e9f9b91a6c7f5bea9a6927d790
rafdiecas <rdiezcastro@safareyes.es>
2025-01-30 12:12:38 +0100

    pagina usuario a falta de botones terminada

--- src/app/features/usuario/components/pagina-usuario/pagina-usuario.component.ts
+++ src/app/features/usuario/components/pagina-usuario/pagina-usuario.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {BotonComponent} from '../../../../shared/components/boton/boton.component';
-import {MatIcon} from '@angular/material/icon';
-import {PerfilUsuarioService} from '../../services/perfil-usuario.service';
-import {DatePipe, NgForOf, NgIf} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PerfilUsuarioService } from '../../services/perfil-usuario.service';
+import { BotonComponent } from '../../../../shared/components/boton/boton.component';
+import { MatIcon } from '@angular/material/icon';
+import {CurrencyPipe, DatePipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-pagina-usuario',
@@ -11,28 +11,66 @@ import {DatePipe, NgForOf, NgIf} from '@angular/common';
     MatIcon,
     NgIf,
     DatePipe,
-    NgForOf
+    NgForOf,
+    CurrencyPipe
   ],
   templateUrl: './pagina-usuario.component.html',
   standalone: true,
   styleUrl: './pagina-usuario.component.css'
 })
 export class PaginaUsuarioComponent implements OnInit {
-
-  isOpen: boolean = false;
-  datosCliente: any;
+  pedidosAbiertos: { [key: number]: boolean } = {};
+  pedidosCargados: { [key: number]: boolean } = {};
+  datosCliente: any = { pedidos: [] };
 
   constructor(private perfilUsuarioService: PerfilUsuarioService) { }
 
   ngOnInit(): void {
-    this.perfilUsuarioService.getDatosCliente().subscribe((data: any) => {
-      console.log(data);
-      this.datosCliente = data;
+    this.cargarDatosCliente();
+  }
+
+  private cargarDatosCliente(): void {
+    this.perfilUsuarioService.getDatosCliente().subscribe({
+      next: (data) => {
+        this.datosCliente = data;
+        if (this.datosCliente.pedidos) {
+          this.datosCliente.pedidos.forEach((pedido: any) => {
+            this.pedidosAbiertos[pedido.id] = false;
+            this.pedidosCargados[pedido.id] = false;
+          });
+        }
+      },
+      error: (err) => console.error('Error cargando datos:', err)
     });
   }
 
-  toggleDropdown(): void {
-    this.isOpen = !this.isOpen;
+  toggleDropdown(pedido: any): void {
+    const pedidoId = pedido.id;
+
+    if (!this.pedidosCargados[pedidoId]) {
+      this.cargarDetallesPedido(pedidoId);
+      this.pedidosCargados[pedidoId] = true;
+    }
+
+    this.pedidosAbiertos[pedidoId] = !this.pedidosAbiertos[pedidoId];
   }
 
+  private cargarDetallesPedido(pedidoId: number): void {
+    this.perfilUsuarioService.getDetallesPedido(pedidoId).subscribe({
+      next: (detalles) => {
+        const pedidoIndex = this.datosCliente.pedidos.findIndex((p: any) => p.id === pedidoId);
+        if (pedidoIndex > -1) {
+          this.datosCliente.pedidos[pedidoIndex].detalles = detalles;
+          console.log('Detalles cargados:', detalles);
+        }
+      },
+      error: (err) => console.error('Error cargando detalles:', err)
+    });
+  }
+
+  calcularTotalPedido(pedido: any): number {
+    return pedido.detalles.reduce((total: number, producto: any) => {
+      return total + (producto.precio * producto.cantidad);
+    }, 0);
+  }
 }
--- src/app/features/usuario/services/perfil-usuario.service.ts
+++ src/app/features/usuario/services/perfil-usuario.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,15 @@ import {HttpClient} from '@angular/common/http';
 export class PerfilUsuarioService {
 
   private apiUrl = 'http://localhost:8000/api/clientes';
+  private apiUrlPedido = 'http://localhost:8000/api/lineaLibro';
 
   constructor(private http: HttpClient) { }
 
-  getDatosCliente(): any {
+  getDatosCliente(): Observable<any> {
     return this.http.get(`${this.apiUrl}/datos`);
+  }
+
+  getDetallesPedido(id:number): Observable<any> {
+    return this.http.get(`${this.apiUrlPedido}/pedidos/${id}`);
   }
 }
