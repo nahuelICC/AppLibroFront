18522a140ea5647dbeae1ae1844fedee29422027
nahuel <ncastrocaceres@safareyes.es>
2025-02-04 19:10:32 +0100

    rango precio responsive

--- src/app/features/tienda/components/rango-precio/rango-precio.component.ts
+++ src/app/features/tienda/components/rango-precio/rango-precio.component.ts
@@ -1,5 +1,5 @@
 // rango-precio.component.ts
-import { Component, Output, EventEmitter } from '@angular/core';
+import {Component, Output, EventEmitter, input, Input, SimpleChanges} from '@angular/core';
 import {MatSlider, MatSliderModule} from '@angular/material/slider';
 import {FormsModule} from '@angular/forms';
 import {NgxSliderModule} from '@angular-slider/ngx-slider';
@@ -18,11 +18,28 @@ import {Options} from '@angular-slider/ngx-slider';
   styleUrls: ['./rango-precio.component.css']
 })
 export class RangoPrecioComponent {
-  minValue: number = 0;
-  maxValue: number = 100;
+  private _minValue: number = 0;
+  private _maxValue: number = 200;
+
+  @Input()
+  set minValue(val: number) {
+    this._minValue = val;
+    // Si es necesario, puedes agregar lógica para reiniciar el slider aquí
+    // Por ejemplo, actualizar alguna variable interna o llamar a una función de reinicialización
+  }
+  get minValue(): number {
+    return this._minValue;
+  }
+
+  @Input()
+  set maxValue(val: number) {
+    this._maxValue = val;
+    // Lógica similar para maxValue
+  }
+  get maxValue(): number {
+    return this._maxValue;
+  }
   @Output() priceChange = new EventEmitter<{ min: number; max: number }>();
-  currentMinValue: number = this.minValue;
-  currentMaxValue: number = this.maxValue;
 
   options: Options = {
     floor: 0,
