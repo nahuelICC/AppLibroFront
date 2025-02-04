d660c2edff44e57539824a4e2f867c6a2e34ed3b
nahuel <ncastrocaceres@safareyes.es>
2025-02-04 13:09:52 +0100

    filtro categorias

---
+++ src/app/features/tienda/components/categorias/categorias.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {GeneroDTO} from '../../DTOs/GeneroDTO';
+import {NgForOf} from '@angular/common';
+import {NombreGeneroPipe} from '../../pipes/nombre-genero.pipe';
+
+@Component({
+  selector: 'app-categorias',
+  imports: [
+    NgForOf,
+    NombreGeneroPipe
+  ],
+  templateUrl: './categorias.component.html',
+  standalone: true,
+  styleUrl: './categorias.component.css'
+})
+export class CategoriasComponent {
+  @Input() categories: GeneroDTO[] = []; // Recibe las categorías desde el padre
+  @Output() categorySelected = new EventEmitter<number | null>();
+  @Input() categoriaSeleccionada: number | null = null;
+
+
+  onCategoria(numero: number): void {
+    this.categorySelected.emit(numero === this.categoriaSeleccionada ? null : numero);
+  }
+}
