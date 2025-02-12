b4872c7baf3c1ae240fbb2d7a3aded28c045872c
nahuel <ncastrocaceres@safareyes.es>
2025-02-12 20:22:37 +0100

    modicar admin para que funcione mejor el componente tabla con una carga perezosa de la tablas

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -5,11 +5,15 @@ import {UsuarioTablaDTO} from './DTO/UsuarioTablaDTO';
 import {ClienteService} from './services/cliente.service';
 import {ClientesTablaDTO} from './DTO/ClientesTablaDTO';
 import { Validators } from '@angular/forms';
+import {PedidoService} from "./services/pedido.service";
+import {PedidosTablaDTO} from "./DTO/PedidosTablaDTO";
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   imports: [
-    TablaComponent
+    TablaComponent,
+    NgIf
   ],
   templateUrl: './admin.component.html',
   standalone: true,
@@ -17,68 +21,108 @@ import { Validators } from '@angular/forms';
 })
 export class AdminComponent implements OnInit{
   titulo: string = 'Clientes';
-  datos: any[] = [];
-  configColumnas: { titulo: string; campo: string, editable: boolean}[] = [];
-  validadores: any;
-  constructor(private clienteService: ClienteService) {
+  activeTabId: string = 'clientes';
+
+  clientesData: any[] = [];
+  pedidosData: any[] = [];
+
+  clientesColumnas: any[] = [];
+  pedidosColumnas: any[] = [];
+
+  clientesValidadores: any = {};
+  pedidosValidadores: any = {};
+
+  clientesCargados: boolean = false;
+  pedidosCargados: boolean = false;
+
+
+  configurarTablaHijo!: (tipoTabla: string) => void;
+
+  constructor(private clienteService: ClienteService,
+              private pedidoService: PedidoService) {
   }
 
   ngOnInit(): void {
-    // Carga por defecto los usuarios
-    this.cargarUsuarios();
-    document.getElementById('clientes')?.classList.remove('hidden');
+    this.cargarClientes();
   }
 
   showTable(tablaId: string, btnActual: EventTarget | null): void {
-    // Ocultar todas las tablas
-    document.getElementById('clientes')?.classList.add('hidden');
-    document.getElementById('pedidos')?.classList.add('hidden');
+    this.activeTabId = tablaId;
+    this.titulo = tablaId === 'clientes' ? 'Clientes' : 'Pedidos';
 
-    // Remover estilos de botón activo
-    document.getElementById('btnUsuarios')?.classList.remove('bg-gray-200');
-    document.getElementById('btnPedidos')?.classList.remove('bg-gray-200',);
-
-    // Mostrar la tabla seleccionada
-    document.getElementById(tablaId)?.classList.remove('hidden');
-
-    // Cambiar título de la sección
-    const titulo = tablaId === 'clientes' ? 'Clientes' : 'Pedidos';
-    const tituloElement = document.getElementById('tituloSeccion');
-    if (tituloElement) {
-      tituloElement.textContent = titulo;
+    if (tablaId === 'clientes' && !this.clientesCargados) {
+      this.cargarClientes();
+      this.clientesCargados = true;
+    } else if (tablaId === 'pedidos' && !this.pedidosCargados) {
+      this.cargarPedidos();
+      this.pedidosCargados = true;
     }
 
-    // Aplicar estilos al botón activo
-    // Aseguramos que btnActual es un HTMLElement
+    this.estiloBotones(btnActual);
+  }
+
+  private estiloBotones(btnActual: EventTarget | null | HTMLElement) {
+    document.getElementById('btnUsuarios')?.classList.remove('bg-gray-200');
+    document.getElementById('btnPedidos')?.classList.remove('bg-gray-200',);
     if (btnActual instanceof HTMLElement) {
-        btnActual.classList.add('bg-gray-200');
+      btnActual.classList.add('bg-gray-200');
     }
   }
 
-  private cargarUsuarios() {
-    document.getElementById('clientes')?.classList.remove('hidden');
-    this.titulo = 'Clientes';
-    // Configuración de columnas para Usuarios
-    this.configColumnas = [
-      { titulo: 'Usuario', campo: 'usuario', editable: false},
-      { titulo: 'Email', campo: 'email', editable: false},
-      { titulo: 'Activo', campo: 'activo', editable: true},
-      { titulo: 'Nombre', campo: 'nombre', editable: true},
-      { titulo: 'Apellido', campo: 'apellido', editable: true},
-      { titulo: 'Telefono', campo: 'telefono', editable: true},
-      { titulo: 'Direccion', campo: 'direccion', editable: true},
-      { titulo: 'Suscrito', campo: 'suscrito', editable: true}
+//carga de tabla Ususarios
+  private cargarClientes() {
+    this.clientesColumnas = [
+      { titulo: 'Usuario', campo: 'usuario', editable: false, isEstado: false},
+      { titulo: 'Email', campo: 'email', editable: false, isEstado: false},
+      { titulo: 'Activo', campo: 'activo', editable: true, isEstado: false},
+      { titulo: 'Nombre', campo: 'nombre', editable: true, isEstado: false},
+      { titulo: 'Apellido', campo: 'apellido', editable: true, isEstado: false},
+      { titulo: 'Telefono', campo: 'telefono', editable: true, isEstado: false},
+      { titulo: 'Direccion', campo: 'direccion', editable: true, isEstado: false},
+      { titulo: 'Suscrito', campo: 'suscrito', editable: true, isEstado: false}
     ];
-    this.validadores = {
+    this.clientesValidadores = {
       nombre: [Validators.required, Validators.minLength(3)],
       apellido: [Validators.required, Validators.minLength(4)],
       telefono: [Validators.required, Validators.pattern(/^\d{9}$/)],
       direccion: [Validators.required]
 
     };
-    // Llamada al servicio para obtener los usuarios
-    this.clienteService.getClientesTabla().subscribe((res: ClientesTablaDTO[]) => {
-      this.datos = res;
+    this.clientesData = [];
+    this.clienteService.getClientesTabla().subscribe({
+      next: (res: ClientesTablaDTO[]) => {
+        this.clientesData = res;
+        this.clientesCargados = true;
+      },
+      error: (err) => {
+        console.error('Error cargando clientes:', err);
+        this.clientesCargados = false; // Permite reintentar
+      }
+    });
+  }
+
+  //carga de tabla pedidos
+  private cargarPedidos() {
+    this.pedidosColumnas = [
+      { titulo: 'Usuario', campo: 'usuario', editable: false, isEstado: false},
+      { titulo: 'Referencia', campo: 'referencia', editable: false, isEstado: false},
+      { titulo: 'Estado', campo: 'estado', editable: true, isEstado: true},
+      { titulo: 'Fecha', campo: 'fecha', editable: false, isEstado: false},
+      { titulo: 'Direccion', campo: 'direccion', editable: false, isEstado: false}
+    ];
+    this.pedidosValidadores = {};
+
+    this.pedidosData = [];
+
+    this.pedidoService.getPedidosTabla().subscribe({
+      next: (res: PedidosTablaDTO[]) => {
+        this.pedidosData = res;
+        this.pedidosCargados = true;
+      },
+      error: (err) => {
+        console.error('Error cargando pedidos:', err);
+        this.pedidosCargados = false;
+      }
     });
   }
 
@@ -88,7 +132,13 @@ export class AdminComponent implements OnInit{
         response => console.log('Usuario actualizado', response),
         error => console.error('Error al actualizar usuario', error)
       );
+    } else if(this.titulo=== 'Pedidos'){
+      this.pedidoService.modificarPedido(item).subscribe(
+        reponse => console.log('Pedido actualizado', reponse),
+        error => console.error('Error al actualizar pedido', error)
+      );
     } else {
+
     }
   }
 }
