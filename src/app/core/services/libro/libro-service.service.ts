f7c21818d97bb49ba796ce7dc86b84c1d949c1aa
rafdiecas <rdiezcastro@safareyes.es>
2025-02-27 16:54:33 +0100

    correciones para despliegue

--- src/app/core/services/libro/libro-service.service.ts
+++ src/app/core/services/libro/libro-service.service.ts
@@ -3,35 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {CuadroProducto} from '../../../features/tienda/DTOs/CuadroProducto';
 import { VerificarCompraResponse } from '../../../features/detalles_libro/DTOs/VerificarCompraResponse';
+import {environment} from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',  // Esto hace que el servicio esté disponible globalmente, sin necesidad de importarlo en módulos
 })
 export class LibroServiceService {
   private apiUrl = '/api/libros';
+  private baseUrl = environment.baseURL;
 
 
   constructor(private http: HttpClient) {}
 
 
   getPrincipal(): Observable<CuadroProducto[]> {
-    return this.http.get<CuadroProducto[]>(`${this.apiUrl}/principal`);
+    return this.http.get<CuadroProducto[]>(`${this.baseUrl}${this.apiUrl}/principal`);
   }
 
   getLibroDetalle(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/detalle/${id}`);
+    return this.http.get<any>(`${this.baseUrl}${this.apiUrl}/detalle/${id}`);
   }
 
   verificarCompra(libroId: number, clienteId: number): Observable<VerificarCompraResponse> {
-    return this.http.get<VerificarCompraResponse>(`${this.apiUrl}/verificarcompra/${libroId}/${clienteId}`);
+    return this.http.get<VerificarCompraResponse>(`${this.baseUrl}${this.apiUrl}/verificarcompra/${libroId}/${clienteId}`);
   }
 
   getNovedades(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/novedades`);
+    return this.http.get(`${this.baseUrl}${this.apiUrl}/novedades`);
   }
 
   getMasVendidos(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/masvendidos`);
+    return this.http.get(`${this.baseUrl}${this.apiUrl}/masvendidos`);
   }
 
 
